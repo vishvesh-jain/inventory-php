e2ed54673698de923e548bd4526115304817e9e8
vishvesh-jain <49018485+vishvesh-jain@users.noreply.github.com>
2019-04-09 19:27:33 +0530

    Add files via upload

---
+++ inventory-master/includes/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| US Technologies Inventory Management System V2
+|--------------------------------------------------------------------------
+| Author: Saeed Nawaz
+| Project Name: USTech-IMS
+| Version: v2
+| Support page: http://ustechlogix.net/
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+?>
